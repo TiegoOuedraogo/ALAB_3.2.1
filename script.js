7b2a0b5ba545d25be675aaeb35449feb82b9acd3
Tiego Ouedraogo <tiegoouedraogo@gmail.com>
2023-11-22 16:47:14 -0500

    first

---
+++ script.js
@@ -0,0 +1,15 @@
+//radius of 5 meters.
+/**
+ * The formula for calculating the area of a circle is PI multiplied by the radius, squared: const 
+ * const PI = 3.1415; 
+ * const area = PI * radius * radius; 
+ * 
+ * Each plant requires a minimum space of 0.8 square meters.
+ * The area is starting with 20 plants.
+ * The plants double in number every week.
+ */
+const radius = 5;
+const PI = 3.1415;
+const area = PI * radius * radius;
+let spaceRequire =0.8; 
+
