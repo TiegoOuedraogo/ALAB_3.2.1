fbb7e2919dbfe03966f8905f61729f1bf975a923
Tiego Ouedraogo <tiegoouedraogo@gmail.com>
2023-11-22 17:11:51 -0500

    Predict the plant growth after a specific number of weeks.

--- script.js
+++ script.js
@@ -11,5 +11,14 @@
 const radius = 5;
 const PI = 3.1415;
 const area = PI * radius * radius;
-let spaceRequire =0.8; 
+let spaceRequire =0.8;//this is in square meter
+let initialPlants =20;
+let maxCapacity = area/spaceRequire; 
 
+let numWeek=1;
+//20 times number of week to the power of 2
+const numPlantAfterNumWeek = initialPlants * Math.pow(2 , numWeek);
+if (numPlantAfterNumWeek > maxCapacity) {
+    numPlantAfterNumWeek = maxCapacity;
+}
+console.console.log(numPlantAfterNumWeek);
